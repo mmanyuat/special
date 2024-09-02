ce8dbb170e611290596979f21e120b3070e82af5
mmanyuat <133497462+mmanyuat@users.noreply.github.com>
2024-09-02 11:41:40 +0000

    added flask project

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def hello_word():
+  return "Hello Guys"
+
+
+if __name__=="__main__":
+  print("I`m inside the if now")
+  app.run(host='0.0.0.0', debug=True)
